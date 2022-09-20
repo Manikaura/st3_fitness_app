cf1bd8f7b223526797525887bc55a7bffcba528c
brainaxe303 <madhav0433.be20@chitkara.edu.in>
2022-09-20 14:07:58 +0530

    finished navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-import App from './App';
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>,
+const App = () => (
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+    </Box>
 );
+
+export default App;
