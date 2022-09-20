30ee0205b6507d3e3d40e7385d2cca1b6b3d1bc6
laksh kataria <lakshkataria@lakshs-MacBook-Pro.local>
2022-09-20 19:03:02 +0530

    adding Bodypart card

---
+++ src/components/BodyPart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BodyPart = () => {
+  return (
+    <div>Bodypart</div>
+  )
+}
+
+export default BodyPart
--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import BodyPart from './BodyPart'
 
-export const HorizontalScrollbar = () => {
+export const HorizontalScrollbar = ({data, bodyPart, setBodyPart }) => {
   return (
-    <div>HorizontalScrollbar</div>
+    <div>
+       {data.map((item) => <Box
+       key={item.id || item}
+       itemId={item.id || item}
+       title={item.id || item}
+       m="0 40px">
+        <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
+            
+       </Box>)}
+    </div>
   )
 }
 export default HorizontalScrollbar;
