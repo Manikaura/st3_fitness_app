fcae2df3de10ab23446a62c9d2d3c4933bb5d0d6
laksh kataria <lakshkataria@lakshs-MacBook-Pro.local>
2022-09-20 19:26:57 +0530

    adding scrolling capabilities

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import React, { useContext } from 'react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { Box, Typography } from '@mui/material';
+
+import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+
+
+const LeftArrow = () => {
+    const { scrollPrev } = useContext(VisibilityContext);
+  
+    return (
+      <Typography onClick={() => scrollPrev()} className="right-arrow">
+        <img src={LeftArrowIcon} alt="right-arrow" />
+      </Typography>
+    );
+  };
+  
+  const RightArrow = () => {
+    const { scrollNext } = useContext(VisibilityContext);
+  
+    return (
+      <Typography onClick={() => scrollNext()} className="left-arrow">
+        <img src={RightArrowIcon} alt="right-arrow" />
+      </Typography>
+    );
+  };
 
 export const HorizontalScrollbar = ({data, bodyPart, setBodyPart }) => {
   return (
-    <div>
-       {data.map((item) => <Box
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+       {data.map((item) => 
+       <Box
        key={item.id || item}
        itemId={item.id || item}
        title={item.id || item}
@@ -13,7 +41,7 @@ export const HorizontalScrollbar = ({data, bodyPart, setBodyPart }) => {
         <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
             
        </Box>)}
-    </div>
+    </ScrollMenu>
   )
 }
 export default HorizontalScrollbar;
